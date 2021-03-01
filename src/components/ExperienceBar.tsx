306843be3875bc59c67aeebb791aec74d1e5853a
claudiogodoy99 <cavalcantigodoy@gmail.com>
2021-03-01 00:58:10 -0300

    new version

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
+import styles from '../styles/components/ExperienceBar.module.css';
 
 const ExperienceBar: React.FC = () => {
+
+    const {currentExperience,experienceToNextLevel} = useContext(ChallengeContext);
+
+    const percentToNextLevel = Math.round((currentExperience * 100)/ experienceToNextLevel);
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{width: '50%'}}/>   
-                <span className="current-experience" style={{left: '50%'}}>300 xp</span>
+                <div style={{width: `${percentToNextLevel}%`}}/>   
+                <span className={styles.currentExperience} style={{left:  `${percentToNextLevel}%`}}>{currentExperience} xp</span>
             </div>
-            <span>600 xp</span>
+            <span>{experienceToNextLevel} xp</span>
         </header>
     );
 }
